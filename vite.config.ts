498ea79a92368c7789cb8b70237b4806409629c5
waoudeco <waoudeco@gmail.com>
2026-09-08 13:39:41 -0500

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import {defineconfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:'https://sabanaflex.com'
-})
