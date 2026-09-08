e66bc94c9a6ba3b1d47cb98ea555efe93f103298
waoudeco <waoudeco@gmail.com>
2026-09-08 12:31:05 -0500

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import {defineconfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base:'https://sabanaflex.com'
+})
